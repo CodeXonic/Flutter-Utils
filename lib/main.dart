ff353a82e1374c91e3889b662f1c778c168b5188
Anshul1507 <ag1507anshul@gmail.com>
2019-06-23 23:08:40 +0530

    Added Alert Dialog Box

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
@@ -25,6 +25,11 @@ class _HomePageState extends State<HomePage> {
 }
 
 class MyBody extends StatelessWidget {
+AlertDialog dialog = new AlertDialog(
+  content: new Text("Dialog build",style: new TextStyle(fontSize: 20.0),),
+  title: new Text("Dialog Box"),
+);
+
   @override
   Widget build(BuildContext context) {
     return  new Container(
@@ -32,14 +37,15 @@ class MyBody extends StatelessWidget {
           child: new RaisedButton(
             child: new Text("Click",style: new TextStyle(color: Colors.white),),
             color: Colors.red,
-            onPressed: () => Scaffold.of(context).showSnackBar(
-              new SnackBar(
-                content: new Text("You clicked me."),
-                duration: new Duration(seconds: 3),
-                )
+            // onPressed: () => Scaffold.of(context).showSnackBar(
+            //   new SnackBar(
+            //     content: new Text("You clicked me."),
+            //     duration: new Duration(seconds: 3),
+            //     )
+            onPressed: ()=> showDialog(context: context,child: dialog ),
             ),
-          ),
-        ),
-      );
+            ),
+          );
+      
   }
 }
